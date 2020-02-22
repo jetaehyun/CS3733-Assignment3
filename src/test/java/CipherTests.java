10a05cdd31c7b16ff8134aaba024b528d1c29a15
jetaehyun <jetaehyun0@gmail.com>
2020-02-22 16:42:16 -0500

    made stuff

--- src/test/java/CipherTests.java
+++ src/test/java/CipherTests.java
@@ -2,5 +2,14 @@
  * Tests for your cipher functions; make sure your code works!
  * Test-driven development is a good idea here.
  */
+
+import edu.wpi.cs3733.entity.ElbonianCipher;
+import org.junit.jupiter.api.Test;
+
+
 public class CipherTests {
+    @Test
+    public void testSomething() {
+        ElbonianCipher elbonianCipher = new ElbonianCipher();
+    }
 }
--- src/test/java/ObserverTests.java
+++ src/test/java/ObserverTests.java
@@ -2,5 +2,17 @@
  * Tests for your observer pattern; make sure your code works!
  * Test-driven development is a good idea here.
  */
+
+import edu.wpi.cs3733.entity.CaesarCipher;
+import org.junit.jupiter.api.Test;
+
+
 public class ObserverTests {
+    @Test
+    private void something() {
+        CaesarCipher d = new CaesarCipher();
+
+    }
+
+
 }
