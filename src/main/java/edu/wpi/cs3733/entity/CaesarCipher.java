bb623120c82ecf72ac46f5eded3b1cc65406c8c0
mchalmers <marcus18ca@gmail.com>
2020-02-22 03:59:21 -0500

    working code translators

--- src/main/java/edu/wpi/cs3733/entity/CaesarCipher.java
+++ src/main/java/edu/wpi/cs3733/entity/CaesarCipher.java
@@ -21,8 +21,180 @@ public class CaesarCipher implements Observer {
 //	}
 
 	public void setText(String text){
-		userText = text;
-		System.out.println(userText);
+			char[] char_array = text.toCharArray();
+			boolean capital;
+			String answerText = "";
+			for (char c : char_array){
+				capital = false;
+				if(Character.toUpperCase(c) == c){
+					capital = true;
+				}
+				char x = Character.toLowerCase(c);
+				switch (x){
+					case 'a':
+						if (capital)
+							answerText += "V";
+						else
+							answerText += "v";
+						break;
+					case 'b':
+						if (capital)
+							answerText += "W";
+						else
+							answerText += "w";
+						break;
+					case 'c':
+						if (capital)
+							answerText += "X";
+						else
+							answerText += "x";
+						break;
+					case 'd':
+						if (capital)
+							answerText += "Y";
+						else
+							answerText += "y";
+						break;
+					case 'e':
+						if (capital)
+							answerText += "Z";
+						else
+							answerText += "z";
+						break;
+					case 'f':
+						if (capital)
+							answerText += "A";
+						else
+							answerText += "a";
+						break;
+					case 'g':
+						if (capital)
+							answerText += "B";
+						else
+							answerText += "b";
+						break;
+					case 'h':
+						if (capital)
+							answerText += "C";
+						else
+							answerText += "c";
+						break;
+					case 'i':
+						if (capital)
+							answerText += "D";
+						else
+							answerText += "d";
+						break;
+					case 'j':
+						if (capital)
+							answerText += "E";
+						else
+							answerText += "e";
+						break;
+					case 'k':
+						if (capital)
+							answerText += "F";
+						else
+							answerText += "f";
+						break;
+					case 'l':
+						if (capital)
+							answerText += "G";
+						else
+							answerText += "g";
+						break;
+					case 'm':
+						if (capital)
+							answerText += "H";
+						else
+							answerText += "h";
+						break;
+					case 'n':
+						if (capital)
+							answerText += "I";
+						else
+							answerText += "i";
+						break;
+					case 'o':
+						if (capital)
+							answerText += "J";
+						else
+							answerText += "j";
+						break;
+					case 'p':
+						if (capital)
+							answerText += "K";
+						else
+							answerText += "k";
+						break;
+					case 'q':
+						if (capital)
+							answerText += "L";
+						else
+							answerText += "l";
+						break;
+					case 'r':
+						if (capital)
+							answerText += "M";
+						else
+							answerText += "m";
+						break;
+					case 's':
+						if (capital)
+							answerText += "N";
+						else
+							answerText += "n";
+						break;
+					case 't':
+						if (capital)
+							answerText += "O";
+						else
+							answerText += "o";
+						break;
+					case 'u':
+						if (capital)
+							answerText += "P";
+						else
+							answerText += "p";
+						break;
+					case 'v':
+						if (capital)
+							answerText += "Q";
+						else
+							answerText += "q";
+						break;
+					case 'w':
+						if (capital)
+							answerText += "R";
+						else
+							answerText += "r";
+						break;
+					case 'x':
+						if (capital)
+							answerText += "S";
+						else
+							answerText += "s";
+						break;
+					case 'y':
+						if (capital)
+							answerText += "T";
+						else
+							answerText += "t";
+						break;
+					case 'z':
+						if (capital)
+							answerText += "U";
+						else
+							answerText += "u";
+						break;
+					case ' ':
+							answerText += " ";
+						break;
+					default:
+						answerText += c;
+				}
+			}
+		userText = answerText;
 	}
 
 	public String getText(){
--- src/main/java/edu/wpi/cs3733/entity/ElbonianCipher.java
+++ src/main/java/edu/wpi/cs3733/entity/ElbonianCipher.java
@@ -23,7 +23,97 @@ public class ElbonianCipher implements Observer {
 	}
 
 	public void setText(String text){
-		userText = text;
+		char[] char_array = text.toCharArray();
+		String answerText = "";
+		for (char c : char_array){
+			c = Character.toLowerCase(c);
+			switch (c){
+				case 'a':
+					answerText += "01";
+					break;
+				case 'b':
+					answerText += "02";
+					break;
+				case 'c':
+					answerText += "03";
+					break;
+				case 'd':
+					answerText += "04";
+					break;
+				case 'e':
+					answerText += "05";
+					break;
+				case 'f':
+					answerText += "06";
+					break;
+				case 'g':
+					answerText += "07";
+					break;
+				case 'h':
+					answerText += "08";
+					break;
+				case 'i':
+					answerText += "09";
+					break;
+				case 'j':
+					answerText += "10";
+					break;
+				case 'k':
+					answerText += "11";
+					break;
+				case 'l':
+					answerText += "12";
+					break;
+				case 'm':
+					answerText += "13";
+					break;
+				case 'n':
+					answerText += "14";
+					break;
+				case 'o':
+					answerText += "15";
+					break;
+				case 'p':
+					answerText += "16";
+					break;
+				case 'q':
+					answerText += "17";
+					break;
+				case 'r':
+					answerText += "18";
+					break;
+				case 's':
+					answerText += "19";
+					break;
+				case 't':
+					answerText += "20";
+					break;
+				case 'u':
+					answerText += "21";
+					break;
+				case 'v':
+					answerText += "22";
+					break;
+				case 'w':
+					answerText += "23";
+					break;
+				case 'x':
+					answerText += "24";
+					break;
+				case 'y':
+					answerText += "25";
+					break;
+				case 'z':
+					answerText += "26";
+					break;
+				case ' ':
+					answerText += "S";
+					break;
+				default:
+					answerText += c;
+			}
+		}
+		userText = answerText;
 	}
 
 	public String getText(){
